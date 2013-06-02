239ed60bb49c5c68d285426b6908e9dff9b613c1
Alexey Selivanov <aseliv@gmail.com>
2013-06-02 05:22:02 +0400

    Project created.

---
+++ src/mtf/Main.java
@@ -0,0 +1,14 @@
+package mtf;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: alexey
+ * Date: 6/2/13
+ * Time: 5:20 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
